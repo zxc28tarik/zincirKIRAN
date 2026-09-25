ad9a50f853be6a55058591e140716f5c2be88fef
zxc28tarik <zxc28tarik@gmail.com>
2026-09-25 16:24:10 +0300

    build: create core package skeleton

---
+++ src/zincir_kiran/__init__.py
@@ -0,0 +1,7 @@
+"""Zincir Kıran core package.
+
+Production logic will be added only after the Research Constitution
+and point-in-time data contracts are enforced by tests.
+"""
+
+__version__ = "0.1.0"
